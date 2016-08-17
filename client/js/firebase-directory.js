e27730d0a60c463009eb70bfb2980139f7b2859b
Philippe Audebaud <paudebau@gmail.com>
2016-08-17 12:43:31 +0200

    change router from ngRouter to ui.router

--- client/js/firebase-directory.js
+++ client/js/firebase-directory.js
@@ -1,57 +1,65 @@
 'use strict';
 
-angular.module('myApp', ['ngTouch', 'ngRoute', 'ngAnimate', 'myApp.firebaseServices'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/employees',
-                            {templateUrl: 'partials/employee-list.html', controller: 'EmployeeListController'});
-        $routeProvider.when('/employees/:employeeId',
-                            {templateUrl: 'partials/employee-detail.html', controller: 'EmployeeDetailController'});
-        $routeProvider.when('/employees/:employeeId/reports',
-                            {templateUrl: 'partials/report-list.html', controller: 'ReportListController'});
-        $routeProvider.otherwise({redirectTo: '/employees'});
+angular.module('factory', ['ngTouch', 'ui.router', 'ngAnimate', 'myApp.firebaseServices'])
+
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise('/employees');
+
+        $locationProvider.html5Mode(true);
+
+        $stateProvider
+            .state('employees',
+                   {url: '/employees', templateUrl: 'partials/employee-list.html', controller: 'EmployeeListController'})
+            .state('details',
+                    {url: '/employees/:employeeId', templateUrl: 'partials/employee-detail.html', controller: 'EmployeeDetailController'})
+            .state('reports',
+                   {url: '/reports/:employeeId', templateUrl: 'partials/report-list.html', controller: 'ReportListController'});
     }])
 
     .controller('EmployeeListController',
                 ['$scope', '$firebaseArray', 'directoryService', EmployeeListController])
     .controller('EmployeeDetailController',
-                ['$scope', '$routeParams', '$firebaseObject', 'directoryService', EmployeeDetailController])
+                ['$scope', '$stateParams', '$firebaseObject', 'directoryService', EmployeeDetailController])
     .controller('ReportListController',
-                ['$scope', '$routeParams', 'reportService', ReportListController])
+                ['$scope', '$stateParams', 'reportService', ReportListController])
 
     .run(['$rootScope', '$window', '$location', 'Auth', function initialize($rootScope, $window, $location, Auth) {
         // Navigation
-        $rootScope.slide = '';        
+        $rootScope.slide = '';
         $rootScope.back = function () { $rootScope.slide = 'slide-right'; $window.history.back(); };
-        $rootScope.go = function (path) { $rootScope.slide = 'slide-left'; $location.url(path); };
+
+        $rootScope.$on('$locationChangeStart', function(event, newUrl, oldUrl) {
+            console.log('go %s ===> %s', oldUrl, newUrl);
+        });
     }]);
 
 function EmployeeListController($scope, $firebaseArray, directoryService) {
     $scope.employees = $firebaseArray(directoryService.ls('employees'));
 }
 
-function EmployeeDetailController($scope, $routeParams, $firebaseObject, directoryService) {
-    var employee = null, employeeId = $routeParams.employeeId;    
+function EmployeeDetailController($scope, $stateParams, $firebaseObject, directoryService) {
+    var employee = null, employeeId = $stateParams.employeeId;
     var employees = directoryService.ls('employees');
     var departments = directoryService.ls('departments');
     employee = new $firebaseObject(employees.child(employeeId));
     employee.$loaded().then(function () { // $on("value", fn) should suffice
         $scope.employee = employee;
-        employee.managerName = '';        
+        employee.managerName = '';
         var dept = new $firebaseObject(departments.child(employee.department));
         dept.$loaded().then(function () {
             employee.city = dept.$value;
             if (employee.managerId !== employeeId) {
                 var manager = new $firebaseObject(employees.child(employee.managerId));
                 manager.$loaded().then(function () {
-                    employee.managerName = manager.firstName + ' ' + manager.lastName;                    
+                    employee.managerName = manager.firstName + ' ' + manager.lastName;
                 });
             }
         });
     });
 }
 
-function ReportListController($scope, $routeParams, reportService) {
-    $scope.employees = reportService.query($routeParams.employeeId);
+function ReportListController($scope, $stateParams, reportService) {
+    $scope.employees = reportService.query($stateParams.employeeId);
 }
 
 
